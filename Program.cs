4d383110a63fdbd673731750a49fba8b9e66e0f4
bryantomoregie <unclebryant@gmail.com>
2020-10-12 22:56:43 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StarterProject 
+{                         
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Cube(5));
+       
+        }
+
+        static int Cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
